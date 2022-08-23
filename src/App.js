066e5fd92a015c2ce471010a76eaac7a3d9bac74
장태준 <jangtaejun0903@gmail.com>
2022-08-23 13:52:51 +0900

    Add: 초기세팅

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Login from './components/Auth/Login';
+import SignUp from './components/Auth/Signup';
+import TodoList from './components/Todo/TodoList';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/signUp" element={<SignUp />} />
+        <Route path="/todo" element={<TodoList />} />
+      </Routes>
     </div>
   );
 }
