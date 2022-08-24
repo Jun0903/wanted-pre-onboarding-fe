fe0b6a447979b4295b35ef12f617955be145bd7c
장태준 <jangtaejun0903@gmail.com>
2022-08-24 16:42:55 +0900

    Fix: 에러로 인한 경로 변경

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import React from 'react';
-import Router from './components/Router';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Login from './components/Auth/Login';
+import SignUp from './components/Auth/SignUp';
+import TodoList from './components/Todo/TodoList';
 
 function App() {
-  return <Router />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/signUp" element={<SignUp />} />
+        <Route path="/todo" element={<TodoList />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
